b8b18a89c9a73631ca5235d3baccd64a776b8076
IndusRobot <whiz_kid10@yahoo.com>
2017-08-16 10:05:02 -0600

    Uploaded Files

---
+++ Spanish-Practice/assets/db_answers_template.js
@@ -0,0 +1,141 @@
+
+
+/*
+Array Template
+
+Use the following terms when defining arrays. Make sure the buttons in the level select div AND the level_select function are correctly linked and defined. 
+
+db_spanish_### = [
+	"",
+	"",
+	"",
+]
+
+db_english_### = [
+	"",
+	"",
+	"",
+]
+
+The first pound sign should either be sophomore or freshman terms. ("S", "F")
+The second should be the chapter number. ("1", "9")
+The third should be the chapter section. ("A", "B")
+
+If there are too many spaces, add more. 
+
+Retrieve any information you might need from: 
+http://www.phschool.com/webcodes10/index.cfm?wcprefix=jck&wcsuffix=0001&area=view
+
+If the provided link does not work, navigate to the correct page from: 
+http://www.phschool.com/atschool/
+
+
+<INSERT> = [
+	"", //0
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //10
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //20
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //30
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //40
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //50
+]
+
+<INSERT> = [
+	"", //0
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //10
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //20
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //30
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //40
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"", //50
+]
+*/
+
